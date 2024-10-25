ee99a22a7a525502d13a5952d14219d506c91986
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-10-25 11:30:14 +0100

    Add project files.

---
+++ Controllers/HIddenNeedsAuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HIddenNeedsAuthController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetValues()
+        {
+            return Ok(new { Value1 = "SecretValue1", Value2 = "SecretValue2" });
+        }
+    }
+}
